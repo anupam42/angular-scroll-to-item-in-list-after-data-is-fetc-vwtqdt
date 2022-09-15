b675b19c0b84c738cf7736b30b54635ea8b674df
anupam42 <anupam.udupi@gmail.com>
2022-09-15 11:40:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-scroll-to-item-in-list-after-data-is-fetc-vwtqdt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-scroll-to-item-in-list-after-data-is-fetc-vwtqdt)
---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component, ViewChildren, ElementRef, QueryList } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { interval, Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  users: any;
+  @ViewChildren('user', { read: ElementRef })
+  renderedUsers: QueryList<ElementRef>;
+  timeoutId = new Subject();
+  selectedId = 9;
+
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((users) => {
+        this.users = users;
+        let index = this.users.map((e) => e.id).indexOf(this.selectedId);
+        this.scrollTo(index);
+      });
+  }
+
+  scrollTo(i: number) {
+    console.log(typeof this.renderedUsers.toArray());
+
+    //this one with setTimeout()
+
+    // this.timeoutId = setTimeout(() => {
+    //   const userToScrollOn = this.renderedUsers.toArray();
+    //   console.log(userToScrollOn);
+    //   userToScrollOn[i].nativeElement.scrollIntoView({
+    //     behavior: 'smooth',
+    //   });
+    // }, 1000);
+
+    //this one with rxjs interval()
+
+    interval(1000)
+      .pipe(take(1), takeUntil(this.timeoutId))
+      .subscribe(() => {
+        const userToScrollOn = this.renderedUsers.toArray();
+        userToScrollOn[i].nativeElement.scrollIntoView({
+          behavior: 'smooth',
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.timeoutId.next(undefined);
+    this.timeoutId.complete();
+  }
+}
